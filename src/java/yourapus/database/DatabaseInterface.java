d1cd27008d8522df23491be58052a6b31baec113
Gab <gabh@protonmail.com>
2018-12-08 16:26:47 +0100

    servlet y html para registro

--- src/java/yourapus/database/DatabaseInterface.java
+++ src/java/yourapus/database/DatabaseInterface.java
@@ -31,62 +31,62 @@ public class DatabaseInterface {
     Listing clasico;
     
     public DatabaseInterface(){
-        Equipo barsa = new Equipo("barsa");
-        Equipo real = new Equipo("real madrid");
-        Equipo betis = new Equipo("betis");
-        Equipo aleti = new Equipo("atleti");
-        Equipo rayo = new Equipo("rayo");
+//        Equipo barsa = new Equipo("barsa");
+//        Equipo real = new Equipo("real madrid");
+//        Equipo betis = new Equipo("betis");
+//        Equipo aleti = new Equipo("atleti");
+//        Equipo rayo = new Equipo("rayo");
+//
+//        ArrayList<Equipo> contrincantes = new ArrayList<Equipo>(
+//            Arrays.asList(barsa, aleti, betis, rayo));
+//
+//        Partido juegoClasico = new Partido(barsa, real);
+//        Partido juegoRealbetis = new Partido(real, betis);
+//        Partido juegoRealRayo = new Partido(real, rayo);
+//
+//        Precios bwinClasico = new Precios("0.15", "0.34", "1.00", "bwin");
+//        Precios bet360Clasico = new Precios("0.10", "0.36", "0.09", "bet360");
+//        Precios casinoClasico = new Precios("1.16", "0.03", "1.40", "casino");
+//        ArrayList<Precios> casasClasico = new ArrayList<Precios>(
+//            Arrays.asList(bwinClasico, bet360Clasico, casinoClasico)
+//        );
+//
+//        Precios bwinRealbetis = new Precios("0.18", "0.32", "0.54", "bwin");
+//        Precios bet360Realbetis = new Precios("0.20", "0.09", "0.19", "bet360");
+//        Precios casinoRealbetis = new Precios("1.15", "1.54", "0.69", "casino");
+//        ArrayList<Precios> casasRealbetis = new ArrayList<Precios>(
+//            Arrays.asList(bwinRealbetis, bet360Realbetis, casinoRealbetis)
+//        );
+//        
+//        Precios bwinRealRayo = new Precios("0.05", "0.14", "3.00", "bwin");
+//        Precios bet360RealRayo = new Precios("0.15", "0.26", "0.49", "bet360");
+//        Precios casinoRealRayo = new Precios("5.16", "0.01", "1.45", "casino");
+//        ArrayList<Precios> casasRealRayo = new ArrayList<Precios>(
+//            Arrays.asList(bwinRealRayo, bet360RealRayo, casinoRealRayo)
+//        );
 
-        ArrayList<Equipo> contrincantes = new ArrayList<Equipo>(
-            Arrays.asList(barsa, aleti, betis, rayo));
 
-        Partido juegoClasico = new Partido(barsa, real);
-        Partido juegoRealbetis = new Partido(real, betis);
-        Partido juegoRealRayo = new Partido(real, rayo);
+//        clasico = new Listing(juegoClasico, casasClasico);
+//        Listing realbetis = new Listing(juegoRealbetis, casasRealbetis);
+//        Listing realRayo = new Listing(juegoRealRayo, casasRealRayo);
 
-        Precios bwinClasico = new Precios("0.15", "0.34", "1.00", "bwin");
-        Precios bet360Clasico = new Precios("0.10", "0.36", "0.09", "bet360");
-        Precios casinoClasico = new Precios("1.16", "0.03", "1.40", "casino");
-        ArrayList<Precios> casasClasico = new ArrayList<Precios>(
-            Arrays.asList(bwinClasico, bet360Clasico, casinoClasico)
-        );
 
-        Precios bwinRealbetis = new Precios("0.18", "0.32", "0.54", "bwin");
-        Precios bet360Realbetis = new Precios("0.20", "0.09", "0.19", "bet360");
-        Precios casinoRealbetis = new Precios("1.15", "1.54", "0.69", "casino");
-        ArrayList<Precios> casasRealbetis = new ArrayList<Precios>(
-            Arrays.asList(bwinRealbetis, bet360Realbetis, casinoRealbetis)
-        );
+//        this.partidos = new ArrayList<Listing>(
+//            Arrays.asList(clasico, realbetis, realRayo));
+//        
+//        this.partidosFavoritos = new ArrayList<Listing>(
+//            Arrays.asList(clasico, realbetis));
         
-        Precios bwinRealRayo = new Precios("0.05", "0.14", "3.00", "bwin");
-        Precios bet360RealRayo = new Precios("0.15", "0.26", "0.49", "bet360");
-        Precios casinoRealRayo = new Precios("5.16", "0.01", "1.45", "casino");
-        ArrayList<Precios> casasRealRayo = new ArrayList<Precios>(
-            Arrays.asList(bwinRealRayo, bet360RealRayo, casinoRealRayo)
-        );
-
-
-        clasico = new Listing(juegoClasico, casasClasico);
-        Listing realbetis = new Listing(juegoRealbetis, casasRealbetis);
-        Listing realRayo = new Listing(juegoRealRayo, casasRealRayo);
-
-
-        this.partidos = new ArrayList<Listing>(
-            Arrays.asList(clasico, realbetis, realRayo));
+//        ArrayList<Equipo> equiposFavoritos = new ArrayList<Equipo>(
+//                Arrays.asList(real, aleti, rayo));
+//        
+//        this.equipos = new ArrayList<Equipo>(
+//                Arrays.asList(real, aleti, rayo, betis));
         
-        this.partidosFavoritos = new ArrayList<Listing>(
-            Arrays.asList(clasico, realbetis));
-        
-        ArrayList<Equipo> equiposFavoritos = new ArrayList<Equipo>(
-                Arrays.asList(real, aleti, rayo));
-        
-        this.equipos = new ArrayList<Equipo>(
-                Arrays.asList(real, aleti, rayo, betis));
-        
-        this.ultimosPartidos =new ArrayList<Listing>(
-            Arrays.asList(clasico, realRayo));
-        
-        this.currentUser = new Usuario("Gab", "gab@gab.com", this.partidosFavoritos, equiposFavoritos);
+//        this.ultimosPartidos =new ArrayList<Listing>(
+//            Arrays.asList(clasico, realRayo));
+//        
+//        this.currentUser = new Usuario("Gab", "gab@gab.com", this.partidosFavoritos, equiposFavoritos);
     }
     
     ArrayList<Equipo> equipos;
@@ -180,7 +180,7 @@ public ArrayList<Listing> getPartidos() throws SQLException{
   
     
    
-     public ArrayList<Listing> getPartidos(String equipo){
+     public ArrayList<Listing> getPartidos(String equipo) throws SQLException{
          ArrayList<Listing> partidosEquipo = new ArrayList<>();
      
          for (Listing partido : this.partidos){
